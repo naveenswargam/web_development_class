602c12c706611fedde30bbed3a6e36363faf792a
naveenswargam <naveenswargam2018@gmail.com>
2023-09-10 16:00:31 +0530

    java folder added

---
+++ test.js
@@ -0,0 +1,148 @@
+// // console.log("rakesh")
+
+
+// // x=5
+// // y =6
+// // x+y
+
+
+// // let x
+// // let y
+// // let z
+// // x=5
+// // y=6
+// // z=x+y
+// // console.log(z)
+
+// //sum of two number
+// // let x=5
+
+// // let y =6
+// // let z=x+y
+// // console.log(z)
+// // let name ="rakesh kumar"
+// // console.log(name)
+// // single quote
+// // let data='rakesh father is hardworking'
+// // double quote
+// // let data='rakesh father is hardworking'
+// // let data="rakesh's father is hardworking"
+// // let  data ="rakesh father is 'hardworking' person"
+
+// let  data1 ='naveen','*/rakesh*/','hardworking','person'
+// let  data2 =`rakesh's father is "hardworking" person`    
+// console.log(data1)
+
+
+// // var x =5dd
+// // console.log("before",x)
+// //  x =6
+// // console.log("after",x)
+
+// // let x =5
+// // console.log("before",x)
+// //  x ="rakesh"
+// // console.log("after",x)
+
+
+// // const x =5
+// // console.log("before",x)
+// //   x =6
+// // console.log("after",x)
+
+
+
+
+// //                redeclare    reassign
+
+// //      var         yes            yes(both are changed)
+// //      let          no            yes (reassign the values)
+// //      const         no            no
+
+
+// // let firstName="Rakesh"
+// // let lastName="Kumar"
+// // let fullName=firstName+ " "+lastName
+// // console.log("fullName",typeof(fullName))
+
+// // let num1=5
+// // let num2=6
+// // let total=num1+num2
+// // console.log("total",typeof(total))
+// // console.log(num1+num2)
+
+// // let name ="rakesh"
+// // let num1  =5
+// // let data=name+num1
+// // console.log("total",data)
+// // console.log("total",typeof(data))
+
+
+// // let num1  =5
+// // let num2=10
+// // let name ="rakesh"
+// // //num+num =number string +string=string string+num =string
+// // // let data=num1+num2+name          //15rakesh   string      5+10=15+"rakesh"="15rakesh"
+// // let data=num1+name +num2            //  5+"rakesh"="5rakesh"+10 ="5rakesh10"
+// // console.log("total",data,typeof(data))     
+
+
+
+// // let num1=5
+// // let num2="6"
+// // console.log(typeof(num1+num2))
+
+// // let name ="rakesh kumar"
+// // console.log(name)
+
+
+
+// // let x=5
+// // let y=6
+// // let z=x+y
+// // console.log(z)
+
+// // let name="naveen"
+// // let num1 =6
+// // let data=name+num1
+// // console.log(data,typeof(data))
+
+// // let x=4
+// // let y=7
+// // let z=x+y
+// // console.log(z,typeof(z))
+
+// // let num1=848
+// // let num2=848
+// // let num3=num1+num2
+// // console.log(num3)
+// // console.log(typeof(num3))
+
+
+// // let x=6
+// // let y=9
+// // let z=x+y
+// // console.log(z,typeof(z))
+
+// // let text= "ahdfkhdffsnnfkfkff";
+// // let length=text.length;
+
+// // let text2="naveen,apple , banana";
+// // let part=text2.slice(12,19);
+// // console.log(part)
+
+// // var x =123
+// // console.log("before",x)
+// // z =333
+// // console.log("after",z)
+
+// // var x=383
+// // console.log("before",x)
+// //  y=838
+// // console.log("after",y)
+
+
+let name1="laptop"
+let name2="desktop"
+console.log(name1+name2)
+console.log(typeof(name1+name2))
---
+++ test1.js
@@ -0,0 +1,14 @@
+// var x=Naveen
+// var y=kumar
+
+// let fname="naveen"
+// let lname="naveen"
+// let newname=fname.concat(lname.toUpperCase())
+// console.log(newname)
+
+// let fname1="naveen"
+// let lname1="swargam"
+// let newname1=fname1.concat(lname1).toUpperCase()
+// console.log(newname1)
+
+
